2190118e98d87618b3dd9b7da89202f3a692e4a0
Justin Reich <justinreich.dev@gmail.com>
2017-08-28 10:53:38 -0500

    Add create and edit views and testing in PostsController, also cleaned up routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,52 +11,25 @@
 |
 */
 
+// The following routes are unrelated to the final Reddit clone example
 Route::get('/', 'HomeController@showWelcome');
-
 Route::get('/uppercase/{word}', 'HomeController@uppercase');
-
 Route::get('/lowercase/{word}', 'HomeController@lowercase');
-
 Route::get('/increment/{number}', 'HomeController@increment');
-
-// Rotue w/ required parameters
 Route::get('/add/{num1}/{num2}', 'HomeController@add');
-
-// Optional parameter w/ default value
-// Route::get('/sayhello/{name}', 'HomeController@showWelcome');
-
-
-
-
-
+Route::get('/sayhello/{name}', 'HomeController@showWelcome');
 Route::get('/rolldice/{guess}', 'HomeController@rolldice');
-
 Route::get('/sample/{str}', 'SampleController@firstLetter');
-
-
 Route::get('/process/{num}', 'SampleController@processNum');
 Route::get('/double/{num}', 'SampleController@doubleNum');
 Route::get('/triple/{num}', 'SampleController@tripleNum');
-
 Route::get('zero', 'HomeController@resetToZero');
 
+// Lecture example resource route
+Route::resource('dogs', 'DogsController');
 
-
-
-// Silliness
-	// Route::get('/dogs', 'DogsController@index');
-	// Route::get('/dogs/create', 'DogsController@create');
-	// Route::post('/dogs', 'DogsController@store');
-	// Route::get('/dogs/{dog}', 'DogsController@show');
-	// Route::get('/dogs/{dog}/edit', 'DogsController@edit');
-	// Route::put('/dogs/{dog}', 'DogsController@update');
-	// Route::delete('/dogs/{dog}', 'DogsController@destroy');
-
-
-// Refactor
-
-	Route::resource('dogs', 'DogsController');
-	Route::resource('posts', 'PostsController');
+// Resource route for reddit clone
+Route::resource('posts', 'PostsController');
 
 
 
